ffe45734fde9185a26924a2c72ab866c19f3fcd1
kangyonggan <kangyonggan@gmail.com>
2019-09-20 09:34:36 +0800

    rename package

--- src/main/java/com/kangyonggan/demo/Application.java
+++ src/main/java/com/kangyonggan/demo/Application.java
@@ -1,4 +1,4 @@
-package com.kangyonggan.restful;
+package com.kangyonggan.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
